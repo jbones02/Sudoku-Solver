0803298f9efc2f17507658cbe638c7c38b7a9542
jbones02 <jaryd02@gmail.com>
2025-02-04 10:52:19 -0800

    initial commit

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,10 +1,14 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    if (puzzleString.length !== 81) {
+      return true;
+    }
+    return false;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    
   }
 
   checkColPlacement(puzzleString, row, column, value) {
---
+++ models/sudoku-puzzle.js
@@ -0,0 +1,7 @@
+class SudokuPuzzle {
+    constructor(puzzleString) {
+        
+    }
+}
+
+exports.SudokuPuzzle = SudokuPuzzle;
