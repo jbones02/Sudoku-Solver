3e0da2da5d94007da6187efbbd8ac8a017cb427c
jbones02 <jaryd02@gmail.com>
2025-02-04 11:53:15 -0800

    feat: started implementing sudoku-puzzle and sudoku-solver classes

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,3 +1,5 @@
+const SudokuPuzzle = require('../models/sudoku-puzzle.js')
+
 class SudokuSolver {
 
   validate(puzzleString) {
@@ -7,20 +9,72 @@ class SudokuSolver {
     return false;
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(sudokuPuzzle, row, val) {
+    const rowToCheck = sudokuPuzzle.squares[row];
+    if (rowToCheck.includes(val)) {
+      return false;
+    }
+    return true;
+  }
+
+  checkColPlacement(sudokuPuzzle, col, val) {
+    let colToCheck = [];
+
+    // Get vals in col being checked
+    for (curRow = 0; curRow < sudokuPuzzle.DIMENSIONS; curRow++) {
+      colToCheck.push(sudokuPuzzle.squares[curRow][col]);
+    }
+
+    if (colToCheck.includes(val)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  getRegionNumber() {
+    /* Region numbering convention:
+          0 1 2
+          3 4 5
+          6 7 8
+    */
+    if (row <= 2) {
+      if (col <=2) {
+        return 0;
+      } else if (col <= 5) {
+        return 1;
+      } else {
+        return 2;
+      }
+    } else if (row <=5) {
+      if (col <=2) {
+        return 3;
+      } else if (col <=5) {
+        return 4;
+      } else {
+        return 5;
+      }
+    } else {
+      if (col <= 2) {
+        return 6;
+      } else if (col <= 5) {
+        return 7;
+      } else {
+        return 8;
+      }
+    }
+  }
+
+  checkRegionPlacement(sudokuPuzzle, row, col, val) {
+
+  }
+
+  solverRecurse(sudokuPuzzle) {
     
-  }
-
-  checkColPlacement(puzzleString, row, column, value) {
-
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
   }
 
   solve(puzzleString) {
-    
+    const sudokuPuzzle = new SudokuPuzzle(puzzleString);
   }
 }
 
--- models/sudoku-puzzle.js
+++ models/sudoku-puzzle.js
@@ -1,6 +1,16 @@
 class SudokuPuzzle {
+    DIMENSIONS = 9;
+    NUM_SQUARES = 81;
+    squares = [];
+
     constructor(puzzleString) {
-        
+        for (curRow = 0; curRow < this.NUM_SQUARES; curRow++) {
+            this.squares.push([])
+            for (curSquare = 0; curSquare < this.DIMENSIONS; curSquare++) {
+                curSquareVal = (curRow * 9) + curSquare
+                this.squares[curRow].push(puzzleString[curSquareVal]);
+            }
+        }
     }
 }
 
